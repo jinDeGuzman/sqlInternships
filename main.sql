312284ca8ae80a3d564f696270109ec89b50c53b
Jin De <77298205+jinDeGuzman@users.noreply.github.com>
2023-01-11 18:56:11 +0000

    Made a line to avg workdays

--- main.sql
+++ main.sql
@@ -9,15 +9,30 @@ CREATE TABLE internships
 );
 
 INSERT INTO internships values
+('Testing', '000-000-000', 0000, 0, false, false),
+('Testing', '000-000-000', 0000, 0, false, false),
+('Testing', '000-000-000', 0000, 0, false, false),
+('Testing', '000-000-000', 0000, 0, false, false),
+('Testing', '000-000-000', 0000, 0, false, false),
+('Testing', '000-000-000', 0000, 0, false, false),
 ('Testing', '000-000-000', 0000, 0, false, false);
+  
+.print
+SELECT * FROM internships;
 
 .print
-SELECT * FROM internships
+.print 'average number of workdays a week ' + k;
+SELECT avg(workdays_a_week) AS avg_workdays_a_week FROM internships;
+.print
 
+
+  
   /*
   Text is the name of the company 
   starting is the starting date
   meet_requirements is if I would meet the requirements to apply
   how much days you would work a week
-  the phone number of the compant
+  the phone number of the company
+
+I could multiply when selecting from
   */
