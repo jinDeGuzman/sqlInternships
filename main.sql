026025023e3427155bff149c4b6009f0e52e8e3f
Jin De <77298205+jinDeGuzman@users.noreply.github.com>
2023-02-09 19:18:20 +0000

    bruhhhh

--- main.sql
+++ main.sql
@@ -5,35 +5,35 @@ CREATE TABLE internships
   starting DATE,
   workdays_a_week INTEGER,
   does_pay BOOLEAN,
-  meet_requirements BOOLEAN,
-  url TEXT
+  meet_requirements BOOLEAN
 );
+
 INSERT INTO internships values
-('Testing', '0', 000000, 0, false, false, 'www.'),
-('Testing', '0', 000000, 0, false, false, 'www.'),
-('Testing', '0', 000000, 0, false, false, 'www.'),
-('Testing', '0', 000000, 0, false, false, 'www.'),
-('Testing', '0', 000000, 0, false, false, 'www.'),
-('Testing', '0', 000000, 0, false, false, 'www.'),
-('Testing', '0', 000000, 0, false, false, 'www.'),
-('Testing', '0', 000000, 0, false, false, 'www.'),
-('Testing', '0', 000000, 0, false, false, 'www.'),
-('Testing', '0', 000000, 0, false, false, 'www.');
+('Kaiser', 'Work from home', 01022023, 5, true, false),
+('Kargo', 'Los Angeles',12312022, 4,  true, false),
+('Netflix', 'Work from home', 01122025, 5, true, false),
+('Beyond Limits', 'Glendale', 01132023, 5, true, false),
+('Rapp Collins', 'Los Angeles', 01202023, 5, false, false),
+('Accerlant', 'Irvine', 01252023, 4, true, false),
+('Avena Drug Delivery', 'Lake Forest', 01132023, 4, true, false),
+('Market Cast', 'Culver City', 11292022, 5, true, false),
+('Edwards Lifesciences', 'Irvine', 01292023, 3, true, false),
+('Roserva Corp', 'Work from home', 12202023, 5, false, false);
 
 .print
 .print 'Internships'
-.print 'Name of internship, Location, Starting date, workdays a week, if it pays, if I meet requirements, link to listing'
+.print 'Name of internship, Location, Starting date posted, workdays a week, if it pays, if I meet requirements '
 
 .print
 SELECT * FROM internships;
 
 .print
 .print 'sorted by alphabet'
-SELECT name, location, starting, workdays_a_week, does_pay, meet_requirements, url FROM internships ORDER BY name ASC;
+SELECT name, location, starting, workdays_a_week, does_pay, meet_requirements FROM internships ORDER BY name ASC;
 
 .print
-.print 'Ones that pay'
-SELECT name, location, starting, workdays_a_week, does_pay, meet_requirements, url FROM internships ORDER BY (does_pay is true) DESC;
+.print 'Sort by the ones hat pay'
+SELECT name, location, starting, workdays_a_week, does_pay, meet_requirements FROM internships ORDER BY (does_pay is true) DESC;
 
 .print
 .print 'average number of workdays a week '
@@ -48,8 +48,4 @@ SELECT sum(meet_requirements) AS sum_meet_requirements FROM internships;
   /*
   Text is the name of the company 
   starting is the starting date
-  meet_requirements is if I would meet the requirements to apply
-  how much days you would work a week
-  the phone number of the company
-  :^)
-  */
+  meet_requirements is if I would 
